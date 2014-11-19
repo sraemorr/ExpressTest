a082e7150b126f51ffb034250594c86524136505
sraemorr <stephanie.hoag@gmail.com>
2014-11-18 17:14:44 -0800

    Initial file commit

---
+++ meadowlark.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Steph on 11/18/2014.
+ */
+var express = require('express');
+
+var app = express();
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple."
+];
+//set up handlebars view engine
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 4000);
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+app.get('/about', function(req, res){
+    var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+    res.render('about', {fortune: randomFortune});
+});
+
+//custom 404 page
+app.use(function(req, res){
+
+    res.status(404);
+    res.render('404');
+});
+
+//custom 500 page
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+});
